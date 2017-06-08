05e2804431b8b7711870c316d047cff0beb374f1
Unknown <vlastoun@gmail.com>
2017-06-08 20:19:25 +0200

    function formData

--- common/models/picture.js
+++ common/models/picture.js
@@ -1,12 +1,30 @@
 // run generator
 const co = require('co');
 const formidable = require('formidable');
+const CONTAINER = 'pictures';
+const PATH = `${__dirname}/../../files/${CONTAINER}/`;
+
+function parseFile(req) {
+  const STAMP = Date.now();
+  let originalName = '';
+  let filePicture;
+  let form = new formidable.IncomingForm();
+  form.parse(req);
+  form.on('fileBegin', (name, file)=>{
+    originalName = file.name;
+    file.name = `${STAMP}_${file.name}`;
+    file.path = `${PATH}${file.name}`;
+    filePicture = file.File;
+    console.log(filePicture);
+  });
+  return Promise.resolve(filePicture);
+}
 
 module.exports = function(picture) {
   picture.upload = function(req, res, postId, cb) {
     co(function* () {
       try {
-        const pic = yield picture.create({description: Date.now()});
+        file = yield parseFile(req);
       } catch (error) {
         console.log(error.details);
       }
