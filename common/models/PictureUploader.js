6cb99cfd59720123daad740d4b86d22d181eef39
Unknown <vlastoun@gmail.com>
2017-06-09 16:02:46 +0200

    one step closer

--- common/models/PictureUploader.js
+++ common/models/PictureUploader.js
@@ -1,4 +1,4 @@
-const formidable = require('formidable');
+var multiparty = require('multiparty');
 const CONTAINER = 'pictures';
 const PATH = `${__dirname}/../../files/${CONTAINER}/`;
 
@@ -13,12 +13,17 @@ module.exports = class PictureUploader {
 
   parseRequest(req) {
     this.request = req;
-    let form = new formidable.IncomingForm();
-    form.parse(this.request);
-    form.on('fileBegin', (name, file)=>{
-      originalName = file.name;
-      file.name = `${this.stamp}_${file.name}`;
-      file.path = `${this.path}${file.name}`;
+    var form = new multiparty.Form({uploadDir: PATH});
+    form.parse(req, function(err, fields, files) {
+      Object.keys(fields).forEach(function(name) {
+        console.log('got field named ' + name);
+      });
+
+      Object.keys(files).forEach(function(name) {
+        console.log('got file named ' + name);
+      });
+
+      console.log('Upload completed!');
     });
   }
 };
