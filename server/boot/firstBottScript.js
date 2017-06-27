ff1f7a09cf332ae52bfb234e2e0d73faf0bd535d
Unknown <vlastoun@gmail.com>
2017-06-27 23:32:24 +0200

    before autentification

--- server/boot/firstBottScript.js
+++ server/boot/firstBottScript.js
@@ -8,14 +8,17 @@ module.exports = function (app) {
   const Category = app.models.category;
   const Role = app.models.role;
   const RoleMapping = app.models.RoleMapping;
+  const ACL = app.models.ACL;
 
   RoleMapping.belongsTo(User);
+  RoleMapping.belongsTo(Role);
+  User.hasMany(Role, {through: RoleMapping, foreignKey: 'principalId'});
   User.hasMany(RoleMapping, {foreignKey: 'principalId'});
   Role.hasMany(User, {through: RoleMapping, foreignKey: 'roleId'});
   
-
   const users = [
     { username: 'vlastoun', email: 'vlastoun@gmail.com', password: 'lopatka' },
+    { username: 'user', email: 'user@gmail.com', password: 'lopatka' },
   ];
 
   const roles = [
@@ -44,20 +47,19 @@ module.exports = function (app) {
     createUsers,
     createRoles,
   ]).then(([users, roles], err) => {
-    const user = users[0];
     const superuser = roles[0];
     const admin = roles[1];
-    admin.principals.create({
-      principalType: RoleMapping.USER,
-      principalId: user.id
-    }, (err, principal)=>{
-      console.log(principal)
-    })
     superuser.principals.create({
-      principalType: RoleMapping.USER,
-      principalId: user.id
+      principalType: 'superuser',
+      principalId: users[0].id
     }, (err, principal)=>{
-      console.log(principal)
+      console.log('superuser', principal)
+    })
+    admin.principals.create({
+      principalType: 'admin',
+      principalId: users[1].id
+    }, (err, principal)=>{
+      console.log('admin', principal)
     })
   });
 };
