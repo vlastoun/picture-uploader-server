7d2d7f292d4c706746e33e423d154fdc57abcffc
Unknown <vlastoun@gmail.com>
2017-07-03 09:32:01 +0200

    remote method for destroy image from database and server database

--- common/models/cloudinary.js
+++ common/models/cloudinary.js
@@ -1,4 +1,27 @@
-
+require('dotenv').config();
+/* eslint-disable camelcase */
 module.exports = function(Cloudinary) {
+  const cloudinary = require('cloudinary');
+  cloudinary.config({
+    cloud_name: process.env.CLOUDI_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
+  /**
+ * delete image from database cloudinary
+ * @param {string} imageId id of image to delete
+ * @param {Function(Error)} callback
+ */
 
+  Cloudinary.deleteImage = function(imageId, callback) {
+    Cloudinary.find({where: {'public_id': imageId}}, (err, data)=>{
+      data.forEach((item)=>{
+        Cloudinary.destroyById(item.id);
+        cloudinary.uploader.destroy(item.public_id, (result)=>{
+          console.log('cloudinary deleted', result);
+        });
+      });
+    });
+    callback(null, {status: 'OK'});
+  };
 };
